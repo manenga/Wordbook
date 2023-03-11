9874554f5dcf963f61e597a6080b6a2107515f1e
Manenga Mungandi <mungandm@hotmail.com>
2023-03-11 14:35:01 +0200

    Initial Commit

---
+++ Wordbook/WordbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordbookApp.swift
+//  Wordbook
+//
+//  Created by Manenga on 2023/03/11.
+//
+
+import SwiftUI
+
+@main
+struct WordbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
